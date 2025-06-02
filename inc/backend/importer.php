0d758c442f38f4841c4f80571ab0b7e9be4d3995
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2025-06-02 13:25:33 +0500

    asdasd

--- inc/backend/importer.php
+++ inc/backend/importer.php
@@ -35,13 +35,127 @@ function xconnect_importer() {
 		),
 		array(
 			'name'       => 'Home 2',
-			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/home1.jpg',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/home2.jpg',
 			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
 			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer.dat',
 			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
 			//'sliders'    => '/inc/backend/data/main/sliders.zip',
 			'pages'      => array(
 				'front_page' => 'Home 2',
+				'blog'       => 'Blog',
+				'shop'       => 'Shop',
+				'cart'       => 'Cart',
+				'checkout'   => 'Checkout',
+				'my_account' => 'My Account',
+			),
+			'menus'      => array(
+				'primary'   => 'main-menu',
+			)
+		),
+		array(
+			'name'       => 'Home 3',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/home3.jpg',
+			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer.dat',
+			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
+			//'sliders'    => '/inc/backend/data/main/sliders.zip',
+			'pages'      => array(
+				'front_page' => 'Home 3',
+				'blog'       => 'Blog',
+				'shop'       => 'Shop',
+				'cart'       => 'Cart',
+				'checkout'   => 'Checkout',
+				'my_account' => 'My Account',
+			),
+			'menus'      => array(
+				'primary'   => 'main-menu',
+			)
+		),
+		array(
+			'name'       => 'Home 4',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/home4.jpg',
+			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer.dat',
+			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
+			//'sliders'    => '/inc/backend/data/main/sliders.zip',
+			'pages'      => array(
+				'front_page' => 'Home 4',
+				'blog'       => 'Blog',
+				'shop'       => 'Shop',
+				'cart'       => 'Cart',
+				'checkout'   => 'Checkout',
+				'my_account' => 'My Account',
+			),
+			'menus'      => array(
+				'primary'   => 'main-menu',
+			)
+		),
+		array(
+			'name'       => 'Home 5',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/home5.jpg',
+			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer.dat',
+			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
+			//'sliders'    => '/inc/backend/data/main/sliders.zip',
+			'pages'      => array(
+				'front_page' => 'Home 5',
+				'blog'       => 'Blog',
+				'shop'       => 'Shop',
+				'cart'       => 'Cart',
+				'checkout'   => 'Checkout',
+				'my_account' => 'My Account',
+			),
+			'menus'      => array(
+				'primary'   => 'main-menu',
+			)
+		),
+		array(
+			'name'       => 'Home 6',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/home6.jpg',
+			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer.dat',
+			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
+			//'sliders'    => '/inc/backend/data/main/sliders.zip',
+			'pages'      => array(
+				'front_page' => 'Home 6',
+				'blog'       => 'Blog',
+				'shop'       => 'Shop',
+				'cart'       => 'Cart',
+				'checkout'   => 'Checkout',
+				'my_account' => 'My Account',
+			),
+			'menus'      => array(
+				'primary'   => 'main-menu',
+			)
+		),
+		array(
+			'name'       => 'One Page',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/one_page.jpg',
+			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer.dat',
+			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
+			//'sliders'    => '/inc/backend/data/main/sliders.zip',
+			'pages'      => array(
+				'front_page' => 'One Page',
+				'blog'       => 'Blog',
+				'shop'       => 'Shop',
+				'cart'       => 'Cart',
+				'checkout'   => 'Checkout',
+				'my_account' => 'My Account',
+			),
+			'menus'      => array(
+				'primary'   => 'main-menu',
+			)
+		),
+		array(
+			'name'       => 'Online Store',
+			'preview'    => get_template_directory_uri().'/inc/backend/data/maintheme/store.jpg',
+			'content'    => get_template_directory_uri().'/inc/backend/data/maintheme/demo-content.xml',
+			'customizer' => get_template_directory_uri().'/inc/backend/data/maintheme/customizer.dat',
+			'widgets'    => get_template_directory_uri().'/inc/backend/data/maintheme/widgets.wie',
+			//'sliders'    => '/inc/backend/data/main/sliders.zip',
+			'pages'      => array(
+				'front_page' => 'Online Store',
 				'blog'       => 'Blog',
 				'shop'       => 'Shop',
 				'cart'       => 'Cart',
