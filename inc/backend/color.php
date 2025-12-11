8ee82cdb5194357cb40262f860c2660d84d0e79e
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2025-12-11 13:24:06 +0500

    sdfsdf

--- inc/backend/color.php
+++ inc/backend/color.php
@@ -27,6 +27,13 @@ if(!function_exists('xconnect_color_scheme')){
         $divider_color      = !empty($divider_color) ? $divider_color : '#FFFFFF14';
         $dark_divider_color = !empty($dark_divider_color) ? $dark_divider_color : '#3835373b';
 
+        // === Font Options ===
+        $body_typo   = xconnect_get_option('body_typo', []);
+        $second_font = xconnect_get_option('second_font', []);
+
+        $body_font_family   = !empty($body_typo['font-family']) ? $body_typo['font-family'] : 'Inter';
+        $second_font_family = !empty($second_font['font-family']) ? $second_font['font-family'] : 'Belleza';
+
 		// Output variables inside :root
 		$color_scheme .= "
 		:root {
@@ -38,6 +45,9 @@ if(!function_exists('xconnect_color_scheme')){
 		    --white-color: {$white_color};
 		    --divider-color: {$divider_color};
 		    --dark-divider-color: {$dark_divider_color};
+		    /* Typography Variables */
+            --font-body: '{$body_font_family}';
+            --font-second: '{$second_font_family}';
 		}
 		";
 
@@ -51,80 +61,40 @@ if(!function_exists('xconnect_color_scheme')){
 }
 add_action('wp_head', 'xconnect_color_scheme');
 
-//Custom Second Font
-if(!function_exists('xconnect_second_font')){
-	function xconnect_second_font(){
-		$second_font = xconnect_get_option( 'second_font', [] );
-		$data_font = '';
 
-		if ( is_array($second_font) && !empty($second_font['font-family']) && $second_font['font-family'] != 'Belleza' ) {
-			$data_font = 
-			'h1, h2, h3, h4, h5, h6,
-			blockquote,
-			.font-second,
-			.link-details,
-			.slide-rev-subtitle,
-			.xptf-btn,
-			select,
-			.main-navigation ul,
-			.page-header,
-			.post-box .post-cat a,
-			.post-box .entry-meta,
-			.post-box .link-box a,
-			.post-box .quote-box .quote-text,
-			.page-pagination li a, .page-pagination li span,
-			.blog-post .tagcloud a,
-			.drop-cap,
-			.post-nav,
-			.comments-area .comment-item .comment-meta .comment-time,
-			.comments-area .comment-item .comment-meta .comment-reply-link,
-			.comment-form .logged-in-as,
-			.widget .tagcloud a,
-			.widget table,
-			.widget .recent-news .post-on,
-			.xp-heading,
-			.xp-flip-box .number-title span,
-			.xp-counter,
-			.xp-counter span,
-			.xp-countdown li span,
-			.line-progress .percent,
-			.project_filters li a,
-			.projects-grid .projects-box .portfolio-info .portfolio-cates,
-			.project-slider .projects-box .portfolio-info .portfolio-cates,
-			.xp-team .team-info span,
-			.member-info li span,
-			.xp-accordions .acc-item .acc-toggle,
-			.xp-tabs .tab-link,
-			.xp-testimonials .t-head,
-			.features-service-wrapper .features-service-item .features-service-content,
-			div.elementor-widget-heading.elementor-widget-heading .elementor-heading-title,
-			.elementor-element .elementor-widget-button .elementor-button,
-			.elementor-default .elementor-widget-text-editor .elementor-drop-cap,
-			.elementor-widget-wp-widget-polylang .lang-item,
-			#lang_choice_wp-widget-polylang,
-			.mmenu-wrapper .mobile_mainmenu,
-			.woocommerce ul.products li.product, .woocommerce-page ul.products li.product,
-			.woocommerce ul.products li.product .added_to_cart, .woocommerce ul.products li.product .product_type_grouped, .woocommerce-page ul.products li.product .added_to_cart, .woocommerce-page ul.products li.product .product_type_grouped,
-			.woocommerce table.shop_table,
-			.woocommerce .quantity .qty,
-			.cart_totals h2,
-			#add_payment_method .cart-collaterals .cart_totals table td, #add_payment_method .cart-collaterals .cart_totals table th,.woocommerce-cart .cart-collaterals .cart_totals table td, .woocommerce-cart .cart-collaterals .cart_totals table th,.woocommerce-checkout .cart-collaterals .cart_totals table td, .woocommerce-checkout .cart-collaterals .cart_totals table th,
-			.woocommerce .site ul.product_list_widget li a:not(.remove),
-			.woocommerce .widget_shopping_cart .cart_list .quantity,
-			.woocommerce .widget_shopping_cart .total strong,.woocommerce.widget_shopping_cart .total strong,
-			.woocommerce .widget_shopping_cart .total .woocommerce-Price-amount,.woocommerce.widget_shopping_cart .total .woocommerce-Price-amount,
-			.woocommerce-mini-cart__buttons a.button.wc-forward,
-			.woocommerce .woocommerce-widget-layered-nav-list,
-			.woocommerce .widget_price_filter .price_slider_amount,
-			.woocommerce .widget_price_filter .price_slider_amount button.button,
-			.product_meta > span,
-			.woocommerce div.product .entry-summary p.price,.woocommerce div.product .entry-summary span.price{ font-family: '.sprintf( $second_font['font-family'] ).';}
-		    ';
-		}
+// Custom Body Font Variable
+if (!function_exists('xconnect_body_font')) {
+    function xconnect_body_font() {
 
-		if( !empty($data_font) ){
-			echo '<style type="text/css">'.$data_font.'</style>';
-		}
-	}
+        $body_typo = xconnect_get_option('body_typo', []);
+        $body_font_family = !empty($body_typo['font-family']) ? $body_typo['font-family'] : 'Inter';
+
+        $css = "
+        :root {
+            --font-body: '{$body_font_family}';
+        }
+        ";
+
+        echo '<style type="text/css">'.$css.'</style>';
+    }
+}
+add_action('wp_head', 'xconnect_body_font');
+
+
+// Custom Second Font Variable
+if (!function_exists('xconnect_second_font')) {
+    function xconnect_second_font() {
+
+        $second_font = xconnect_get_option('second_font', []);
+        $second_font_family = !empty($second_font['font-family']) ? $second_font['font-family'] : 'Belleza';
+
+        $css = "
+        :root {
+            --font-second: '{$second_font_family}';
+        }
+        ";
+
+        echo '<style type="text/css">'.$css.'</style>';
+    }
 }
 add_action('wp_head', 'xconnect_second_font');
